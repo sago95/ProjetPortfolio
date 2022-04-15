c73bcbae7868215d77911ceb340295d472eddfe4
Moussa Sago Sidibe <moussa.sidibe@odc.local>
2022-04-15 10:53:05 +0000

    mise à jour

--- message.js
+++ message.js
@@ -15,7 +15,7 @@ btn.addEventListener('click', function (e) {
 
     Email.send({
         Host: "smtp.gmail.com",
-        Username: nom,
+        Username: "moussasagos@gmail.com",
         Password: "okuvxguscwmzqqmv",
         To: 'moussasagos@gmail.com',
         From: email,
